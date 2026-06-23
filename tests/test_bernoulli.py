2793296aa20696c5d8108aff21802faf52280310
gianmarco00 <gianmarco.albano@uzh.ch>
2026-06-23 17:20:34 +0200

    added bernoulli test

--- tests/test_bernoulli.py
+++ tests/test_bernoulli.py
@@ -27,3 +27,17 @@ def test_bernoulli_log_prob_returns_correct_value():
 
     assert torch.allclose(result, torch.full((batch_size,), expected_value))
 
+def test_bernoulli_log_prob_returns_correct_value():
+
+    batch_size = 4
+    img_size = 28
+
+    large_value = 10000.0
+    x_zero = torch.zeros(batch_size, 1, img_size, img_size)
+    x_one = torch.ones(batch_size, 1, img_size, img_size)
+    logits = torch.full((batch_size, 1, img_size, img_size), large_value)
+
+    result_zero = bernoulli.log_prob_from_logits(x_zero, logits)
+    result_one = bernoulli.log_prob_from_logits(x_one, logits)
+
+    assert torch.all(result_one > result_zero), "Log probability for x=1 should be greater than for x=0 when logits are large positive values."
