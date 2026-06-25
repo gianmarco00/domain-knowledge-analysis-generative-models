96d09fc0cb0641435b83858acb886a905298f936
gianmarco00 <gianmarco.albano@uzh.ch>
2026-06-25 19:21:23 +0200

    decoder

---
+++ src/domain_knowledge_analysis/models/vae/decoder.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+
+
+class Decoder(nn.Module):
+    def __init__(self, params):
+        super(Decoder, self).__init__()
+
+        layers = []
+
+        kernels = params["kernels"]
+        in_channels = params["in_channels"]
+        out_channels = params["out_channels"]
+        strides = params["strides"]
+        paddings = params["paddings"]
+        output_paddings = params["output_paddings"]
+        fc_neurons = params["fc_neurons"]
+        start_shape = params["last_shape_before_flattening"]
+
+        self.fc = nn.Linear(params["latent_dim"], fc_neurons)
+
+        self.unflatten = nn.Unflatten(
+            dim=1,
+            unflattened_size=start_shape,
+        )
+
+        for n_layer, (in_ch, out_ch, k, s, p, op) in enumerate(
+            zip(
+                in_channels,
+                out_channels,
+                kernels,
+                strides,
+                paddings,
+                output_paddings,
+            )
+        ):
+            layers.append(
+                nn.ConvTranspose2d(
+                    in_channels=in_ch,
+                    out_channels=out_ch,
+                    kernel_size=k,
+                    stride=s,
+                    padding=p,
+                    output_padding=op,
+                )
+            )
+
+            if n_layer < len(out_channels) - 1:
+                layers.append(nn.BatchNorm2d(out_ch))
+                layers.append(nn.LeakyReLU())
+
+        self.conv_transpose = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = self.unflatten(x)
+        x = self.conv_transpose(x)
+
+        return x
+        
---
+++ src/domain_knowledge_analysis/models/vae/encoder.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    def __init__(self, image_shape, params):
+        super(Encoder, self).__init__()
+
+        layers = []
+
+        kernels = params["kernels"]
+        out_channels = params["out_channels"]
+        strides = params["strides"]
+        paddings = params["paddings"]
+
+        in_channels = [image_shape[0], *out_channels[:-1]]
+
+        for in_ch, out_ch, k, s, p in zip(
+            in_channels, out_channels, kernels, strides, paddings
+        ):
+            layers.append(
+                nn.Conv2d(
+                    in_channels=in_ch,
+                    out_channels=out_ch,
+                    kernel_size=k,
+                    stride=s,
+                    padding=p,
+                )
+            )
+            layers.append(nn.BatchNorm2d(out_ch))
+            layers.append(nn.LeakyReLU())
+
+        self.conv = nn.Sequential(*layers)
+        self.flatten = nn.Flatten(start_dim=1)
+
+        # Store intermediate shapes using dummy pass
+        was_training = self.conv.training
+        self.conv.eval()
+        with torch.no_grad():
+            dummy = torch.zeros(image_shape).unsqueeze(0)  
+
+            self.intermediate_shapes = []
+            self.intermediate_shapes.append(dummy.shape[1:])
+
+            for layer in self.conv:
+                dummy = layer(dummy)
+
+                if isinstance(layer, nn.Conv2d):
+                    self.intermediate_shapes.append(dummy.shape[1:])
+
+            self.last_shape_before_flattening = self.intermediate_shapes[-1]
+            self.fc_neurons = dummy.flatten(start_dim=1).shape[1]
+        self.conv.train(was_training)
+
+        self.fc_mean = nn.Linear(self.fc_neurons, params["latent_dim"])
+        self.fc_log_variance = nn.Linear(self.fc_neurons, params["latent_dim"])
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.flatten(x)
+
+        mean = self.fc_mean(x)
+        log_variance = self.fc_log_variance(x)
+
+        return mean, log_variance
