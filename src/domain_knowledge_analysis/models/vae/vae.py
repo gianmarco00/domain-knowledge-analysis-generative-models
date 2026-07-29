bc60a0f2403ccbd79d249811834a12df60c50e65
gianmarco00 <gianmarco.albano@uzh.ch>
2026-07-29 16:29:59 +0200

    asymm results

--- src/domain_knowledge_analysis/models/vae/vae.py
+++ src/domain_knowledge_analysis/models/vae/vae.py
@@ -21,7 +21,9 @@ class Vae(nn.Module):
         if decoder_params is None:
             decoder_params = self.derive_decoder_params_from_encoder(self.encoder, encoder_params) 
 
-        self.decoder = Decoder(decoder_params) if symmetric_decoder else AsymmetricDecoder(decoder_params)
+        self.decoder = Decoder(decoder_params) if symmetric_decoder else AsymmetricDecoder(decoder_params["latent_dim"])
+
+        print("symmetric: f{symmetric_decoder}")
 
     def forward(self, x):
         mean, log_variance = self.encoder(x)
